c0551e2b3b37f7738752b6b114a1fe5a9f38cf11
Md Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-11-18 15:16:41 +0600

    Initialize mongodb connection & middleware like express, cors

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vcmcsxr.mongodb.net/?retryWrites=true&w=majority`;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // Create Database
+    const db = client.db('changemakersdb');
+    // Create Collection
+    const userCollection = db.collection("users");
+
+    // Create API's
+    app.post('/user', async (req, res) => {
+      const data = req.body;
+      try {
+        const result = await userCollection.insertOne(data); 
+        res.send({
+          success: true,
+          insertedId: result.insertedId
+        });
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ success: false, error: err.message });
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close(); // Don't close connection if server should keep running
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send("Together, Make Bangladesh Great!")
-})
+  res.send("Together, Make Bangladesh Great!");
+});
 
 app.listen(port, () => {
-  console.log(`ChangeMakers Backend app listening on port ${port}`)
-})
+  console.log(`ChangeMakers Backend app listening on port ${port}`);
+});
